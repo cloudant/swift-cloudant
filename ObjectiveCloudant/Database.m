310cfe961532ccf8e4d395a2da4b1e77a874817e
Rhys Short <rhyshort@uk.ibm.com>
2015-09-23 10:44:44 +0100

    Correct copyright

--- ObjectiveCloudant/Database.m
+++ ObjectiveCloudant/Database.m
@@ -3,6 +3,7 @@
 //  ObjectiveCouch
 //
 //  Created by Michael Rhodes on 15/08/2015.
+//  Copyright (c) 2015 IBM Corp.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
 //  except in compliance with the License. You may obtain a copy of the License at
