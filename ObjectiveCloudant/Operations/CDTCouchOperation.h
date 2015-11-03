9b02098cf2694a770302fdc37865692b33aaf546
Rhys Short <rhyshort@uk.ibm.com>
2015-11-03 16:31:14 +0000

    Correct copyright headers

--- ObjectiveCloudant/Operations/CDTCouchOperation.h
+++ ObjectiveCloudant/Operations/CDTCouchOperation.h
@@ -3,6 +3,7 @@
 //  ObjectiveCouch
 //
 //  Created by Michael Rhodes on 27/08/2015.
+//  Copyright (c) IBM Corp. 2015
 //
 //  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
 //  except in compliance with the License. You may obtain a copy of the License at
