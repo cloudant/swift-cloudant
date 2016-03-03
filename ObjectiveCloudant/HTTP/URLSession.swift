c9bd5092773fc6b2c69cc4f4cc08ff08077e06a7
Rhys Short <rhys.short@gmail.com>
2016-03-03 20:32:13 +0000

    Inital URLSession port

---
+++ ObjectiveCloudant/HTTP/URLSession.swift
@@ -0,0 +1,155 @@
+//
+//  URLSession.swft
+//  ObjectiveCloudant
+//
+//  Created by Rhys Short on 28/02/2016.
+//  Copyright © 2016 Small Text. All rights reserved.
+//
+
+import Foundation
+
+
+
+protocol HTTPInterceptor
+{
+    func interceptRequest(ctx:HTTPInterceptorContext) -> HTTPInterceptorContext
+    func interceptResponse(ctx:HTTPInterceptorContext) -> HTTPInterceptorContext
+}
+
+extension HTTPInterceptor {
+    
+    func interceptRequest(ctx:HTTPInterceptorContext) -> HTTPInterceptorContext {
+        return ctx;
+    }
+    
+    func interceptResponse(ctx:HTTPInterceptorContext) -> HTTPInterceptorContext {
+        return ctx;
+    }
+}
+
+public struct HTTPInterceptorContext {
+    let request:NSMutableURLRequest
+    let response: NSHTTPURLResponse?
+    var shouldRetry:Bool = false
+}
+
+public class URLSessionTask {
+    private let request:NSURLRequest
+    private var inProgessTask: NSURLSessionDataTask?
+    private let session:NSURLSession
+    private var remainingRetries:Int = 10
+    private let interceptors:Array<HTTPInterceptor>
+    private let completionHandler: (NSData?, NSURLResponse?, NSError?) -> (Void)
+    
+    public var state:NSURLSessionTaskState {
+        get {
+            if let task = self.inProgessTask {
+                return task.state
+            } else {
+                return .Suspended
+            }
+        }
+    }
+    
+    init(session:NSURLSession, request:NSURLRequest, interceptors:Array<HTTPInterceptor>, completionHandler:(NSData?, NSURLResponse?, NSError?) -> (Void)){
+        self.interceptors = interceptors
+        self.request = request
+        self.session = session
+        self.completionHandler = completionHandler
+    }
+    
+    public func resume(){
+        if let task = inProgessTask {
+            task.resume();
+        }else {
+            //make task and start
+            let task = self.makeRequest()
+            self.inProgessTask = task
+            task.resume()
+        }
+    }
+    
+    public func cancel() {
+        if let task = self.inProgessTask {
+            task.cancel()
+        }
+    }
+    
+    
+    private func makeRequest() -> NSURLSessionDataTask {
+        var ctx = HTTPInterceptorContext(request: request.mutableCopy() as! NSMutableURLRequest, response: nil, shouldRetry: false)
+        
+        for interceptor in interceptors {
+            ctx = interceptor.interceptRequest(ctx)
+        }
+        
+        return self.session.dataTaskWithRequest(request, completionHandler: { (data, response, error) -> Void in
+            ctx = HTTPInterceptorContext(request: ctx.request, response: (response as! NSHTTPURLResponse), shouldRetry: ctx.shouldRetry)
+            
+            for interceptor in self.interceptors {
+                ctx = interceptor.interceptResponse(ctx)
+            }
+            
+            if ctx.shouldRetry && self.remainingRetries > 0 {
+                self.remainingRetries--;
+                self.inProgessTask = self.makeRequest()
+                self.inProgessTask?.resume()
+            } else {
+                //call completion
+            }
+        })
+    }
+    
+    
+}
+
+public class InterceptableSession {
+    
+    
+    private let session:NSURLSession
+    private let interceptors:Array<HTTPInterceptor>
+    
+    
+    convenience init(){
+        self.init(delegate: nil,requestInterceptors: [])
+    }
+    
+    init(delegate:NSURLSessionDelegate?, requestInterceptors:Array<HTTPInterceptor>){
+        interceptors = requestInterceptors
+        
+        let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+        config.HTTPAdditionalHeaders = ["User-Agent":InterceptableSession.userAgent()]
+        session = NSURLSession(configuration: config,delegate:delegate, delegateQueue: nil)
+    }
+    
+    
+    public func dataTask(request:NSURLRequest, completionHandler:(NSData?, NSURLResponse?, NSError?) -> Void ) -> URLSessionTask
+    {
+        return URLSessionTask(session: session, request: request, interceptors: interceptors, completionHandler: completionHandler)
+    }
+    
+    deinit {
+        self.session.finishTasksAndInvalidate()
+    }
+    
+    private class func userAgent() -> String {
+        let processInfo = NSProcessInfo.processInfo()
+        let osVersion = processInfo.operatingSystemVersionString
+        
+        #if os(iOS)
+            let platform = "iOS"
+        #elseif os(OSX)
+            let platform = "OSX"
+        #else
+            let platform = "Unkown";
+        #endif
+        
+        let frameworkBundle = NSBundle(forClass: InterceptableSession.self)
+        let bundleDisplayName = frameworkBundle.objectForInfoDictionaryKey("CFBundleName")
+        let bundleVersionString = frameworkBundle.objectForInfoDictionaryKey("CFBundleShortVersionString")
+        
+        return "\(bundleDisplayName)/\(bundleVersionString) (\(platform), \(osVersion))"
+    }
+    
+    
+}
---
+++ SessionCookieInterceptor.swift
@@ -0,0 +1,148 @@
+//
+//  SessionCookieInterceptor.swift
+//  ObjectiveCloudant
+//
+//  Created by Rhys Short on 28/02/2016.
+//  Copyright © 2016 Small Text. All rights reserved.
+//
+
+import Foundation
+
+
+public struct SessionCookieInterceptor : HTTPInterceptor
+{
+    
+    private let sessionCookieTimeout:Int64 = 600
+    private let sessionRequestBody:NSData
+    private var shouldMakeSessionRequest:Bool = true
+    private var cookie:String?
+    private let urlSession:InterceptableSession
+    
+    
+    init(username:String, password:String){
+        let encodedUsername = username.stringByAddingPercentEncodingWithAllowedCharacters
+        let encodedPassword = password.stringByAddingPercentEncodingWithAllowedCharacters
+        
+        let payload = "name=\(encodedUsername)&password=\(encodedPassword)"
+        
+        sessionRequestBody = payload.dataUsingEncoding(NSASCIIStringEncoding)!
+        urlSession = InterceptableSession()
+  
+    }
+    
+    
+    mutating func interceptRequest(ctx: HTTPInterceptorContext) -> HTTPInterceptorContext {
+        // if we shouldn't make the request just return th ctx
+        guard shouldMakeSessionRequest
+            else {
+                return ctx
+        }
+        
+        if let cookie = self.cookie {
+            //apply the coode
+            ctx.request.setValue(cookie, forHTTPHeaderField: "Cookie")
+        } else {
+            // get the new cookie and apply it
+            self.cookie = self.startNewSession(ctx.request.URL!)
+            if let cookie = self.cookie {
+                ctx.request.setValue(cookie, forHTTPHeaderField: "Cookie")
+            }
+        }
+        
+        return ctx
+    }
+    
+    
+    private mutating func startNewSession(url:NSURL) -> String? {
+        let components = NSURLComponents(URL: url, resolvingAgainstBaseURL: false)!
+        components.path = "/_session"
+        
+        let request = NSMutableURLRequest(URL: components.URL!)
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.HTTPMethod = "POST"
+        request.HTTPBody = self.sessionRequestBody
+        
+        
+        let semaphore = dispatch_semaphore_create(0)
+        var cookie:String?
+        
+        let task = self.urlSession.dataTask(request) { (data, response, error) -> Void in
+            
+            //defer semaphore
+            defer { dispatch_semaphore_signal(semaphore) }
+            
+            let response = response as? NSHTTPURLResponse
+            
+            if let error = error {
+                //Network failure, often transient, try again next time around.
+                NSLog("Error making cookie response, error \(error.localizedDescription)");
+            }
+            
+            if let response = response {
+                //we have a response
+                
+                if response.statusCode / 100 == 2 {
+                    //success
+                    
+                    guard let data = data
+                        else {
+                            NSLog("No data from response, bailing")
+                            return;
+                    }
+                    
+                    //Check data sent back before attempting to get the cookie from the headers.
+                    do {
+                        let jsonResponse = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions())
+                        
+                        // Only check for ok:true, https://issues.apache.org/jira/browse/COUCHDB-1356
+                        // means we cannot check that the name returned is the one we sent.
+                        guard let ok = jsonResponse.objectForKey("ok") as? NSNumber where
+                            ok.boolValue
+                            else {
+                                NSLog("Response did not contain ok:true, bailing")
+                                return
+                        }
+                        
+                        guard let cookieHeader = response.allHeaderFields["Set-Cookie"] as? String
+                            else {
+                                // log error and return
+                                NSLog("Cookie header was no present or could not be parsed.")
+                                return
+                        }
+                        
+                        cookie = cookieHeader.componentsSeparatedByString(";").first
+
+                    } catch {
+                        // log it and error out.
+                        NSLog("Failed to deserialise data as json, error: \(error)")
+                    }
+                    
+                } else if response.statusCode == 401 {
+                    // Creds invalid, fail don't retry.
+                    // Credentials are not valid, fail and don't retry.
+                    NSLog("Credentials are incorrect, cookie authentication will not be attempted again by this interceptor");
+                    self.shouldMakeSessionRequest = false
+                } else if response.statusCode / 100 == 5 {
+                    // Server error of some kind; often transient. Try again next time.
+                    NSLog("Failed to get cookie from the server, response code was \(response.statusCode).")
+                } else {
+                    // Most other HTTP status codes are non-transient failures; don't retry.
+                    NSLog("Failed to get cookie from the server,response code \(response.statusCode). Cookie authentication will not be attempted again by this interceptor object");
+                    self.shouldMakeSessionRequest = false
+                }
+ 
+            }
+        }
+        
+        dispatch_semaphore_wait(semaphore, dispatch_time(DISPATCH_TIME_NOW, self.sessionCookieTimeout * Int64(NSEC_PER_SEC)))
+        
+        
+        return cookie
+        
+        
+        
+    }
+    
+    
+    
+}
