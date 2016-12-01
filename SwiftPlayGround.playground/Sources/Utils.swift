ce1ba975fd35902fe48fc4579bd1dc72051d307a
Rhys Short <rhyshort@uk.ibm.com>
2016-12-01 16:53:07 +0000

    More docs

--- SwiftPlayGround.playground/Sources/Utils.swift
+++ SwiftPlayGround.playground/Sources/Utils.swift
@@ -2,6 +2,10 @@ import Foundation
 
 
 
+/// Generate documents in the form ["foo":"bar", "number": x] where x is generated.
+///
+/// - Parameter count: The number of documents to generate
+/// - Returns: Generated documents.
 public func generateDocuments(count: Int) -> [[String:Any]] {
     var documents:[[String:Any]] = []
     
